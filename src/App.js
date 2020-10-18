47eb944bc6c2b3bfb47708b5ca5879a53fabbeaf
Michael <Mick666@hotmail.com.au>
2020-10-19 02:50:55 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { gql, useQuery } from '@apollo/client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ALL_AUTHORS = gql`
+query {
+  allAuthors  {
+    name
+    bookCount
+  }
+}
+`
+
+const App = () => {
+    const result = useQuery(ALL_AUTHORS)
+
+    if (result.loading) {
+        return <div>loading...</div>
+    }
+
+    return (
+        <div>
+            {result.data.allAuthors.map(p => p.name).join(', ')}
+        </div>
+    )
 }
 
-export default App;
+export default App
